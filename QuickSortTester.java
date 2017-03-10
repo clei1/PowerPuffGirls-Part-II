1992ca1dce5a0bddc8083f027e10c3445b5ff277
yedohkang913 <ykang@stuy.edu>
2017-03-09 22:42:45 -0500

    Update QuickSortTester.java

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,7 +1,7 @@
-//PowerPuff Girls (Connie Lei, Yedoh Kang, Donia Tung)
-//APCS2 pd1
-//Lab#01--What Does The Data Say?
-//2017-03-09
+// PowerPuff Girls (Connie Lei, Yedoh Kang, Donia Tung)
+// APCS2 pd1
+// Lab#01 -- What Does The Data Say?
+// 2017-03-09
 
 /*======================================
   class QuickSortTester
@@ -10,28 +10,33 @@
   O(nlogn)
 
   Mean execution times for dataset of size n:
-  look in csv!!!
+  Look in test.csv!
   
   n=<huge>  time: nlogn
 
   ANALYSIS:
-  On average, the run time is nlogn because the sort function itself runs recursively with a logn runtime, and it calls partition, which has an n runtime. Everytime we iterate through, we look at the two partitioned pieces, and the partition value is left in it's final resting place. We continue to do this on each partitioned piece until the values are all in their final resting places. In the best case, the partition value would be the median of the array, so the array would split directly in half. In the worst case, the partition value would be an extreme of the values in the data set, so the array would have to partition n times and would take n squared. 
-  
-
+  On average, the run time is nlogn because the sort function itself runs recursively with a O(logn) runtime, 
+  	and it calls partition, which has an O(n) runtime. Everytime we iterate through the array, we look at the two 
+	partitioned parts of the array, and the partition value is left in its final resting place. We continue to 
+	do this on each partitioned part until the values are all in their final resting places. 
+  In the best case, the partition value would be the median of the array, so the array would split directly in half. 
+  In the worst case, the partition value would be the extreme of the values in the array, so it would have to partition 
+  	n times, therefore making it a O(n^2) runtime. 
+ 	 
   =======================================*/
+
 import java.util.*;
 import java.io.*;
 
 public class QuickSortTester 
 {
-
     /******************************
      * execution time analysis 
-     Depending on the lengths of arrays we wanted to choose, we put those lengths into an int[] array and then, populate an array with numbers, then mess up the array,
-     start the timer, run the sort program, end the timer and then average it all.
+     Depending on the lengths of arrays we wanted to choose, we put those lengths into an int[] array and then, 
+     	populate an array with numbers, then mess up the array, start the timer, run the sort program, 
+	end the timer and then average it all.
      ******************************/
     public static void main( String[] args ) {
-
 	try{
 	    PrintWriter outputStream = new PrintWriter("test.csv");
 	    int[] lengths = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 30, 35, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 200, 300, 400, 450, 500, 550, 600, 700, 800, 1000};
@@ -56,12 +61,10 @@ public class QuickSortTester
 		//System.out.println("Average time for array length " + a.length + " is " + avg);
 		outputStream.print(a.length + ", " + avg + "\n");
 	    }
-
 	    outputStream.close();
 	}
 	catch(Exception e){
 	    System.out.println("Something went wrong");
 	}
     }//end main
-
 }//end class
